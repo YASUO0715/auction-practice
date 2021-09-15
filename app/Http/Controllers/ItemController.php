f8465e5c428ce9e49f96d3f831111cbc94fa9711
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-15 15:49:01 +0900

    オークションアプリのデータの更新、削除を追加しました

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -16,11 +16,68 @@ class ItemController extends Controller
         return view('items.index', ['items' => $items]);
     }
 
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $item = new Item;
+
+        // 値の用意
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->seller = $request->seller;
+        $item->email = $request->email;
+        $item->image_url = $request->image_url;
+
+        // インスタンスに値を設定して保存
+        $item->save();
+
+        // 登録したらindexに戻る
+        return redirect('/items');
+    }
 
     // showページへ移動
     public function show($id)
     {
         $item = Item::find($id);
         return view('items.show', ['item' => $item]);
+    }
+
+    public function edit($id) 
+     {
+         $item = Item::find($id);
+         return view('items.edit', ['item' => $item]);
+     }
+
+     public function update(Request $request, $id) 
+     {
+         // ここはidで探して持ってくる以外はstoreと同じ
+         $item = Item::find($id);
+
+         // 値の用意
+         $item->name = $request->name;
+         $item->description = $request->description;
+         $item->price = $request->price;
+         $item->seller = $request->seller;
+         $item->email = $request->email;
+         $item->image_url = $request->image_url;
+
+         // 保存
+         $item->save();
+
+         // 登録したらindexに戻る
+         return redirect('/items');
+     }
+
+     public function destroy($id)
+    {
+        $item = Item::find($id);
+        $item->delete();
+        return redirect('/items');
     }
 }
